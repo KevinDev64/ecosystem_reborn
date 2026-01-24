293fa0820e351b94c080047722f2ffcf825dc70e
KevinDev64 <kevindev56@yandex.ru>
2026-01-24 19:09:19 +0300

    feat/software: setup setting menu & catching first starting

--- software/main/main.ino
+++ software/main/main.ino
@@ -9,6 +9,7 @@
 #include <Wire.h> // I2C lib
 #include <LiquidCrystal_I2C.h> // LCD lib 
 #include <EEPROM.h> // Arduino EEPROM lib
+#include <string.h> // string type
 
 
 // constant global values section 
@@ -31,6 +32,14 @@
 #define BUTTONS_PIN A0 // Left, Right, OK, Cancel analog buttons
 #define SETUP_JUMPER 10 // If on setup will appear
 
+// EEPROM init values
+#define EEPROM_INIT_ADDR 1023
+#define EEPROM_INIT_KEY 77
+
+// Buttons flags
+bool left_button_flag, right_button_flag;
+bool ok_button_flag, esc_button_flag;
+bool* buttons_array[4] {&left_button_flag, &right_button_flag, &ok_button_flag, &esc_button_flag};
 
 // Default threshold values (can be changed in setup)
 float air_temp_min_crit = 17.0;
@@ -53,6 +62,10 @@ bool vent_in_flag, vent_out_flag;
 bool unknown_device_flag; // (can be used later)
 
 
+// Timers (for millis)
+uint64_t screen_timer;
+uint64_t buttons_timer;
+
 // Define sensors & LCD
 OneWire oneWire(GROUND_TEMP_SENSOR_PIN);
 DallasTemperature ground_temp_sensor(&oneWire);
@@ -61,7 +74,7 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 
 // const arrays for loops
-const int relays_pins[6] = {
+const int relays_pins[8] = {
   DAY_LIGHT_RELAY_PIN,
   NIGHT_LIGHT_RELAY_PIN,
   AIR_HEATER_RELAY_PIN,
@@ -70,6 +83,19 @@ const int relays_pins[6] = {
   VENT_IN_RELAY_PIN,
   VENT_OUT_RELAY_PIN,
   UNKNOWN_RELAY_A_PIN
+};
+
+// arrays for settings 
+const String settings_names_array[8] {"air_temp_min_crit", "air_temp_min", "air_temp_max", "air_temp_max_crit", "ground_temp_min", "ground_temp_max", "ground_hum_min", "ground_hum_max"};
+float* settings_values_table[8] {
+  &air_temp_min_crit,
+  &air_temp_min,
+  &air_temp_max,
+  &air_temp_max_crit,
+  &ground_temp_min,
+  &ground_temp_max,
+  &ground_hum_min,
+  &ground_hum_max
 };
 
 void setup() {
@@ -92,8 +118,122 @@ void setup() {
   pinMode(BUTTONS_PIN, INPUT); 
   pinMode(SETUP_JUMPER, INPUT_PULLUP);
 
+  // SETUP
+  if (EEPROM.read(EEPROM_INIT_ADDR) != EEPROM_INIT_KEY) {
+    // first start => setup jumper must be setted
+    while (digitalRead(SETUP_JUMPER) != 0) {
+      lcd.print("ecosystem v1.0");
+      lcd.setCursor(0, 1);
+      lcd.print("--------------------");
+      lcd.setCursor(0, 2);
+      lcd.print("SET UP JMP_SETUP!");
+      lcd.setCursor(0, 3);
+      lcd.print("waiting...");
+    }
+
+    // show info message and start editor after delay
+    lcd.clear();
+    lcd.print("Press RESET");
+    lcd.setCursor(0, 1);
+    lcd.print("after editing");
+    delay(3000); 
+
+    void set_default_values();
+    void setup_settings();
+  }
 }
 
 void loop() {
 
+}
+
+void setup_settings() {
+  // Settings menu for changing threshold values
+  int setting_index = 0;
+  screen_timer = millis();
+  buttons_timer = millis();
+
+  // LCD update timer
+  if (millis() >= (screen_timer + 1000)) {
+    screen_timer = millis(); 
+    lcd_print_setup_settings(setting_index);
+  }
+
+  // Buttons update timer
+  if (millis() >= (buttons_timer + 100)) {
+    buttons_timer = millis();
+    get_control_buttons_values();
+    if (left_button_flag) { 
+      *settings_values_table[setting_index] -= 0.1; 
+      // TODO: find index in setting_names_array and write new value to EEPROM
+    }
+    if (right_button_flag)  *settings_values_table[setting_index] += 0.1;
+    if (ok_button_flag)     {
+      if (setting_index == 7) { setting_index = 0; }
+      else { setting_index += 1; }
+    }
+    if (esc_button_flag) {
+      if (setting_index == 0) { setting_index = 7; }
+      else { setting_index -= 1; }
+    }
+  }
+  
+}
+
+void lcd_print_setup_settings(int setting_index) {
+  lcd.clear();
+  lcd.print(settings_names_array[setting_index]);
+  lcd.setCursor(0, 1);
+  lcd.print("Value: ");
+  lcd.print(*settings_values_table[setting_index]);
+  lcd.setCursor(0, 2);
+  lcd.print("L(-0.1)  R(+0.1)");
+  lcd.setCursor(0, 3);
+  lcd.print("OK-next  ESC-prev");
+} 
+
+void get_control_buttons_values() {
+  uint16_t rawButtonsValue = analogRead(BUTTONS_PIN);
+  if (rawButtonsValue >= 0 and rawButtonsValue <= 5) {
+    for (int i = 0; i <= 3; i++) {
+      *buttons_array[i] = false;
+    }
+  }
+  if (rawButtonsValue >= 200 and rawButtonsValue <= 210) {
+    for (int i = 0; i <= 3; i++) {
+      *buttons_array[i] = false;
+    }
+    esc_button_flag = true;
+  }
+  if (rawButtonsValue >= 405 and rawButtonsValue <= 415) {
+    for (int i = 0; i <= 3; i++) {
+      *buttons_array[i] = false;
+    }
+    ok_button_flag = true;
+  }
+  if (rawButtonsValue >= 609 and rawButtonsValue <= 619) {
+    for (int i = 0; i <= 3; i++) {
+      *buttons_array[i] = false;
+    }
+    right_button_flag = true;
+  }
+  if (rawButtonsValue >= 813 and rawButtonsValue <= 823) {
+    for (int i = 0; i <= 3; i++) {
+      *buttons_array[i] = false;
+    }
+    left_button_flag = true;
+  }
+}
+
+void set_default_values() {
+  EEPROM.write(0, air_temp_min_crit);
+  EEPROM.write(4, air_temp_min);
+  EEPROM.write(8, air_temp_max);
+  EEPROM.write(12, air_temp_max_crit);
+  EEPROM.write(16, ground_temp_min);
+  EEPROM.write(20, ground_temp_max);
+  EEPROM.write(24, ground_hum_min);
+  EEPROM.write(28, ground_hum_max);
+
+  EEPROM.write(1023, EEPROM_INIT_KEY);
 }
